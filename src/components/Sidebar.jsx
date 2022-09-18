fdcbd34b2c5afae0d0c1036c24951babaa9215f9
Milton Luiz <milton.luiz36@hotmail.com>
2022-09-17 21:41:15 -0400

    Finalização da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,19 @@
 import styles from './Sidebar.module.css';
 
+import { PencilLine } from 'phosphor-react'
+
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
 
-            <img className={styles.cover} 
-            src='https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=
+            <img className={styles.cover}
+                src='https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=
             rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=
             500&q=50'/>
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src='https://github.com/coutmilton.png' />
+
                 <strong>Milton Luiz</strong>
                 <span>React Developer</span>
 
@@ -17,6 +21,7 @@ export function Sidebar() {
 
             <footer>
                 <a href='#'>
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
