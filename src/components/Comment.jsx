9778101c85a098645ad6620dd6a3050db41c7dc6
Milton Luiz <milton.luiz36@hotmail.com>
2022-09-18 13:58:27 -0400

    Adicionando os comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/coutmilton.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Milton Luiz</strong>
+                            <time title="17 de setembro ás 08:13h" dateTime="2022-09-17 08:13:13">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Meus parabens</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
